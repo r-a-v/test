b56f40ea19ead78eba6efb180f2056679cf753bb
Alexander Rudikov <rav.lnk1991@gmail.com>
2021-09-06 15:07:38 +0700

    Initial Commit

---
+++ test_QEX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test_QEX
+//
+//  Created by aleksandr_rudikov on 06.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
